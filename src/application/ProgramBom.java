d1ae7bdeb6a957ddd01adc685fc615d0d95a0cac
Mauricio Oliveira <mauricioliveira13@hotmail.com>
2021-02-11 11:52:15 -0300

    Projeto criado

---
+++ src/application/ProgramBom.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import model.entities.ReservationBom;
+import model.exceptions.DomainException;
+
+public class ProgramBom {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			System.out.print("Room Number: ");
+			int number = sc.nextInt();
+	
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(sc.next());
+	
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(sc.next());
+	
+			ReservationBom reservation = new ReservationBom(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+	
+			System.out.println();
+			System.out.println("Enter the update data: ");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+	
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+	
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+		}
+		catch(ParseException e){
+			System.out.println("Invalid date format");
+		}
+		catch(DomainException e) {
+			System.out.println("Error in reservation: " + e.getMessage());
+		}
+		
+		catch(RuntimeException e) {
+			System.out.println("Unexpected error");
+		}
+		sc.close();
+	}
+
+}
